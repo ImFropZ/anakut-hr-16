204629409eb9e5ec70c24816610e4633f933ac00
Lim Tangmeng <limtangmeng100@gmail.com>
2023-08-18 15:13:24 +0700

    add: search filter for approval module

--- anakut_hr/__manifest__.py
+++ anakut_hr/__manifest__.py
@@ -6,11 +6,12 @@
     'author': 'Your Name',
     'website': 'https://www.yourwebsite.com',
     'license': 'LGPL-3',
-    'depends': ['base', 'hr', 'hr_contract', 'hr_payroll', 'hr_work_entry_contract_enterprise'],  # List of dependencies
+    'depends': ['base', 'hr', 'hr_contract', 'hr_payroll', 'hr_work_entry_contract_enterprise', 'approvals'],  # List of dependencies
     'data': [
         'security/hr_employee_security.xml',
         'security/ir.model.access.csv',
     
+        'views/approval_views.xml',
         'menu/op_menu.xml',
     ],
     'installable': True,
